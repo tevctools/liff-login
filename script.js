8aa6801038b2d2d95f9c00ba791fe7575b625b15
tevctools <tevcev02@gmail.com>
2025-05-29 14:35:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const liffId = '2005677310-7VNqpWnW'; // 你的 LIFF ID
 async function main() {
   await liff.init({ liffId });
 
+  // 不判斷登入狀態，直接呼叫 login()
   if (!liff.isLoggedIn()) {
-    liff.login();  // 還沒登入就叫他登入
-  } else {
+    liff.login();
+    return;
+  }
+
+  try {
     const profile = await liff.getProfile();
     const userId = profile.userId;
 
-    // 登入後導回你的官網 + 帶入 line_id 參數
-   window.location.href = `https://www.tevc.com.tw/item/register?line_id=${userId}`;
+    // 跳轉到 redirect.html，夾帶 line_id
+    window.location.href = `https://tevctools.github.io/liff-login/redirect.html?line_id=${userId}`;
+  } catch (err) {
+    console.error("取得 profile 失敗，可能沒有授權：", err);
+    liff.logout();
+    liff.login(); // 強制重新登入
   }
 }
 
